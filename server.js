8d2901735e71cf08de45ce8fcd2a5fb96cc139de
Jan Szewczyk <36161455+IvanPlease@users.noreply.github.com>
2021-10-25 03:44:31 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const port = process.env.PORT || 4001;
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = socketIo(server, { cors: { orgin: "http://localhost/3000" } });
+
+io.on("connect", (socket) => {
+
+    console.log(`Connected: ${socket.id}`);
+
+    socket.on('disconnect', () =>
+       console.log(`Disconnected: ${socket.id}`));
+       
+    socket.on('join', (room) => {
+        console.log(`Socket ${socket.id} joining ${room}`);
+        socket.join(room);
+    });
+
+    socket.on('sendCommand', (data, rooms) => {
+        rooms.forEach(element => {
+            console.log(`Socket ${socket.id} sent command to ${element}`)
+            io.to(element).emit('recieveCommand', data);
+        });
+    });
+
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
