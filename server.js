022a815acec487bed8fe86056eef7d9a2fbbe5b5
Jan Szewczyk <36161455+IvanPlease@users.noreply.github.com>
2021-10-25 03:45:09 +0200

    Test

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 const server = http.createServer(app);
 
-const io = socketIo(server, { cors: { orgin: "http://localhost/3000" } });
+const io = socketIo(server, { cors: { orgin: "https://ivanplease.github.io/harcmiliada-front/" } });
 
 io.on("connect", (socket) => {
 
