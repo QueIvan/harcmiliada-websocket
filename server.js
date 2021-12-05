98283670fc45c9c22643b17591b2303685821e8f
Jan Szewczyk <36161455+QueIvan@users.noreply.github.com>
2021-12-05 13:15:55 +0100

    [Socket] 2.0 Implementation

--- server.js
+++ server.js
@@ -3,36 +3,37 @@ const http = require("http");
 const socketIo = require("socket.io");
 
 const port = process.env.PORT || 4001;
-
 const app = express();
-
 const server = http.createServer(app);
 
 const io = socketIo(server, { cors: { orgin: "*" } });
 
 io.on("connect", (socket) => {
+	console.log(`Połączono: ${socket.id}`);
 
-    console.log(`Connected: ${socket.id}`);
+	socket.on("join", (roomName, gameId) => {
+		socket.join(`${roomName}-&${gameId}`);
+		console.log(`${socket.id} dołączył do pokoju ${roomName}-#${gameId}`);
+		socket.emit("joined", `${roomName}-#${gameId}`);
+	});
 
-    socket.on('disconnect', () =>
-       console.log(`Disconnected: ${socket.id}`));
-       
-    socket.on('join', (room) => {
-        console.log(`Socket ${socket.id} joining ${room}`);
-        socket.join(room);
-    });
+	socket.on("setVisibilityStatus", (roomName, data) => {
+		socket.to(roomName).emit("setVisibilityStatus", data);
+	});
 
-    socket.on('sendCommand', (data, rooms) => {
-        rooms.forEach(element => {
-            console.log(`Socket ${socket.id} sent command to ${element}`)
-            io.to(element).emit('recieveCommand', data);
-        });
-    });
+	socket.on("setAnswerVisibility", (roomName, data) => {
+		socket.to(roomName).emit("setAnswerVisibility", data);
+	});
 
-    socket.on('pickSide', (data) => {
-        io.to("boards").emit('recieveCommand', {type: "pickSide", side: data});
-    })
+	socket.on("setWrongAnswersCount", (roomName, data) => {
+		socket.to(roomName).emit("setWrongAnswersCount", data);
+	});
 
+	socket.on("reloadBoard", (roomName) => {
+		socket.to(roomName).emit("reloadBoard");
+	});
+
+	socket.on("disconnect", () => console.log(`Rozłączono: ${socket.id}`));
 });
 
-server.listen(port, () => console.log(`Listening on port ${port}`));
+server.listen(port, () => console.log(`Nasłuchuję port: ${port}`));
